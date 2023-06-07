d5b437ae85c011f2064169f32823435a8a6de2e9
Devaansh Bhandari <107868772+brf153@users.noreply.github.com>
2023-06-07 15:48:17 +0530

    Update axios.js

--- tinder-frontend/src/axios.js
+++ tinder-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://tinder-clone-umber.vercel.app/'
+    baseURL: 'https://tinder-clone-jgtuo4uka-brf153.vercel.app/
 })
 
 export default instance
